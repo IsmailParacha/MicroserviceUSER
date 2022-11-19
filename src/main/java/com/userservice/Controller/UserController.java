df8fe6aa0e4465f78e01bd29c4e44349457237da
IsmailParacha <ismailjanparacha@gmail.com>
2022-11-20 00:12:14 +0500

    controller created and successfull User Api created

---
+++ src/main/java/com/userservice/Controller/UserController.java
@@ -0,0 +1,66 @@
+package com.userservice.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.userservice.Paylod.ApiResponse;
+import com.userservice.Paylod.UserDto;
+import com.userservice.Services.UserServices;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+    @Autowired
+    private UserServices userServices;
+
+    // POST-create user
+    @PostMapping("/add/")
+    public ResponseEntity<UserDto> createUser(@Validated @RequestBody UserDto userdto) {
+        UserDto createUserDto = this.userServices.createUser(userdto);
+        return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+    }
+
+    // PUT-update user
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity<UserDto> updateUser(@Validated @RequestBody UserDto userdto, @PathVariable Integer id) {
+        UserDto updateUser = this.userServices.updateUser(userdto, id);
+        return ResponseEntity.ok(updateUser);
+
+    }
+
+    // DELETE-delete user
+    @DeleteMapping("/deleteUser/id:{id}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer id) {
+        this.userServices.deleteUser(id);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted successfully", true), HttpStatus.OK);
+    }
+
+    // GET- multiple users get
+    @GetMapping("/getUsers/")
+    public ResponseEntity<List<UserDto>> getAllUsers(
+            @RequestParam(value = "pageNo", defaultValue = "1", required = false) Integer pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "2", required = false) Integer pageSize) {
+        List<UserDto> users = this.userServices.getAllUsers(pageNo, pageSize);
+        return new ResponseEntity<List<UserDto>>(users, HttpStatus.OK);
+    }
+
+    // GET- single user get
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<UserDto> getSingleUsers(@PathVariable Integer id) {
+        return ResponseEntity.ok(this.userServices.getUser(id));
+    }
+
+}
---
+++ src/main/java/com/userservice/Paylod/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.userservice.Paylod;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String messgae;
+    private boolean success;
+
+}
