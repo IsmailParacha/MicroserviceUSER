71119bdf51f47d7566ffd586f9f2ef27b71d9a02
IsmailParacha <ismailjanparacha@gmail.com>
2022-11-19 23:49:42 +0500

    Model mapper added

--- src/main/java/com/userservice/Service1Application.java
+++ src/main/java/com/userservice/Service1Application.java
@@ -1,13 +1,20 @@
 package com.userservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Service1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service1Application.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
